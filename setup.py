f5cbe633fa86238bbebb19fa01b258913149615e
Frankie Dintino <fdintino@theatlantic.com>
2012-02-22 15:14:04 -0500

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='django-mysqlndb-backend',
+    version='1.0.0',
+    author_email='ATMOprogrammers@theatlantic.com',
+    packages=['mysqlndb'],
+    url='https://github.com/theatlantic/django-mysqlndb-backend',
+    description='Provides a django database backend that works with MySQL Cluster\'s NDB storage engine.',
+    install_requires=[
+    	"Django >= 1.2",
+        "MySQL-python >= 1.2.2",
+    ],
+)
