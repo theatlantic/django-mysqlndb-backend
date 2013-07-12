a3ce1656310d1c1abd7362e98e41476d0edcba1e
Frankie Dintino <fdintino@theatlantic.com>
2013-07-12 15:41:48 -0400

    Backport support of ForeignKey.db_constraint to django < 1.6

--- mysqlndb/creation.py
+++ mysqlndb/creation.py
@@ -1,3 +1,5 @@
+import copy
+
 import MySQLdb as Database
 from django.db.backends.mysql import creation
 
@@ -11,7 +13,7 @@ class DatabaseCreation(creation.DatabaseCreation):
         self.supports_foreign_keys = getattr(self.connection.features, "supports_foreign_keys", True)
 
     def sql_for_inline_foreign_key_references(self, field, known_models, style):
-        if not self.supports_foreign_keys:
+        if not self.supports_foreign_keys or not getattr(field, 'db_constraint', True):
             return [], False
         return super(DatabaseCreation, self).sql_for_inline_foreign_key_references(field, known_models, style)
 
@@ -23,4 +25,10 @@ class DatabaseCreation(creation.DatabaseCreation):
     def sql_for_pending_references(self, model, style, pending_references):
         if not self.supports_foreign_keys:
             return []
+        elif model in pending_references:
+            references = copy.deepcopy(pending_references)
+            pending_references = []
+            for rel_class, f in references:
+                if getattr(f, 'db_constraint', True):
+                    pending_references.append((rel_class, f))
         return super(DatabaseCreation, self).sql_for_pending_references(model, style, pending_references)
--- mysqlndb/south_backend.py
+++ mysqlndb/south_backend.py
@@ -1,7 +1,22 @@
 from south.db.mysql import DatabaseOperations as BaseDatabaseOperations
 
+
 class DatabaseOperations(BaseDatabaseOperations):
+
     def connection_init(self):
         super(DatabaseOperations, self).connection_init()
         connection = self._get_connection()
         self.supports_foreign_keys = getattr(connection.features, "supports_foreign_keys", True)
+
+    def alter_column(self, table_name, name, field, *args, **kwargs):
+        if not getattr(field, 'db_constraint', True):
+            kwargs['ignore_constraints'] = True
+        return super(DatabaseOperations, self).alter_column(table_name, name, field, *args, **kwargs)
+
+    def column_sql(self, table_name, field_name, field, *args, **kwargs):
+        supports_foreign_keys = self.supports_foreign_keys
+        if not getattr(field, 'db_constraint', True):
+            self.supports_foreign_keys = False
+        retval = super(DatabaseOperations, self).column_sql(table_name, field_name, field, *args, **kwargs)
+        self.supports_foreign_keys = supports_foreign_keys
+        return retval
